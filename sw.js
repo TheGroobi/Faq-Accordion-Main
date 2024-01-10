98be6555ac8f661199b6c37fd37f78ca17edb8a7
Szymon W. <23wilk.szymon@gmail.com>
2024-01-10 19:47:35 +0100

    to do: Change svg onclick

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
 const section = document.querySelectorAll('.container');
+// const myFrag = new DocumentFragment();
+// let row = document.createElement(minus)
+
 const hideOnClick = (e => e.currentTarget
     .nextElementSibling
     .classList.toggle('hidden'));
 
 section.forEach(container => container.addEventListener('click', hideOnClick));
-
 //change svg for minus
-// figure out how to do animation from display: none;
+//listen for click
+//select closes hover-button element
+//insertAdjecentHTML element
