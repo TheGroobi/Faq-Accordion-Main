631a7d1e98a9397a2e917112ccd233586937d662
Szymon W. <23wilk.szymon@gmail.com>
2024-01-10 07:07:52 +0100

    asdasdasdasds

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-console.log('it works')
+//listen for click on .container
+//add class to all p besides the one near to hidden
+//add class of border to all sections beside the one clicked
+//change svg for minus
+// //add animation
+function hideOnClick() {
+    const section = document.querySelectorAll('.container');
+    const toggleBorder = section.forEach(container => container.classList.toggle('border'));
+    console.log(toggleBorder);
+    section.forEach(
+        container => container.addEventListener('click', e => console.log(e.currentTarget)))
+};
